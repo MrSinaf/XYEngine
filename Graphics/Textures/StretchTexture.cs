ea39eca0aa9fe977570a5c02ab7cd2ca34bdcb29
MrSinaf <mrsinaf@sinafproduction.xyz>
2024-10-28 14:09:44 +0100

    Add scale & TextureAtlas support in StretchTexture.cs

--- Graphics/Textures/StretchTexture.cs
+++ Graphics/Textures/StretchTexture.cs
@@ -8,21 +8,29 @@ public class StretchTexture
     public readonly Vector2Int cornerSize00;
     public readonly Vector2Int cornerSize11;
 
-    public StretchTexture(Texture texture, RectInt corner00, RectInt corner11)
+    public StretchTexture(Texture texture, RectInt corner00, RectInt corner11, int scale = 1)
     {
         this.texture = texture;
-        cornerSize00 = corner00.position11 - corner00.position00;
-        cornerSize11 = corner11.position11 - corner11.position00;
+        cornerSize00 = (corner00.position11 - corner00.position00) * scale;
+        cornerSize11 = (corner11.position11 - corner11.position00) * scale;
         
         this.corner00 = new Rect(corner00.position00.ToVector2() * texture.texelSize, corner00.position11.ToVector2() * texture.texelSize);
         this.corner11 = new Rect(corner11.position00.ToVector2() * texture.texelSize, corner11.position11.ToVector2() * texture.texelSize);
     }
 
-    [Obsolete("Il est au fait pas encore implémenté !")]
-    public StretchTexture(TextureAtlas texture, Rect target, RectInt border)
+    public StretchTexture(TextureAtlas texture, string target, RectInt corner00, RectInt corner11, int scale = 1)
     {
+        texture.data.frames.TryGetValue(target, out var frame);
         this.texture = texture;
-        corner00 = new Rect(target.position00, target.position00 + border.position00.ToVector2() * texture.texelSize);
-        corner00 = new Rect(target.position11, target.position11 + border.position11.ToVector2() * texture.texelSize);
+        corner00.position00 += frame.position;
+        corner00.position11 += frame.position;
+        corner11.position00 += frame.position;
+        corner11.position11 += frame.position;
+        
+        cornerSize00 = (corner00.position11 - corner00.position00) * scale;
+        cornerSize11 = (corner11.position11 - corner11.position00) * scale;
+        
+        this.corner00 = new Rect(corner00.position00.ToVector2() * texture.texelSize, corner00.position11.ToVector2() * texture.texelSize);
+        this.corner11 = new Rect(corner11.position00.ToVector2() * texture.texelSize, corner11.position11.ToVector2() * texture.texelSize);
     }
 }
