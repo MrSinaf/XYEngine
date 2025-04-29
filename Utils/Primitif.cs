bf3a915dc061d7176949bf1015eb0073b7ae3e78
MrSinaf <mrsinaf@sinafproduction.xyz>
2025-04-29 21:13:06 +0200

    Add quad mesh initialization in Primitif class
    - Ajout d'une propriété `quad` de type `Mesh` dans `Primitif`.

--- Utils/Primitif.cs
+++ Utils/Primitif.cs
@@ -5,10 +5,13 @@ namespace XYEngine.Utils;
 public static class Primitif
 {
 	public static Texture2D whitePixel { get; private set; }
+	public static Mesh quad { get; private set; }
 	
 	public static void Init()
 	{
 		whitePixel = new Texture2D(1, 1, [Color.white]);
 		whitePixel.Apply();
+		
+		quad = MeshFactory.CreateQuad(Vector2.one).Apply();
 	}
 }
