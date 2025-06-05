6b6c2ef8dd09c459d15ad79e441a3663db36f282
MrSinaf <mrsinaf@sinafproduction.xyz>
2025-06-05 17:54:37 +0200

    Add animation system implementation (｡♥‿♥｡)
    > Introduction d'un système d'animation dans le moteur.
    > Ajout des classes principales : `Animation`, `AnimationTrack<T>` et `KeyFrame<T>`.
    > Gestion des animations avec support pour le bouclage, les événements de fin et l'interpolation.
    > Permet d'ajouter, supprimer et mettre à jour des pistes d'animation dynamiquement.

---
+++ Core/Animations/Animation.cs
@@ -0,0 +1,73 @@
+namespace XYEngine.Animations;
+
+public class Animation
+{
+	private readonly List<IAnimationTrack> animationTracks = [];
+	
+	public bool isRunning { get; private set; }
+	public float duration { get; private set; }
+	public float currentTime { get; private set; }
+	
+	public bool loop;
+	
+	public event Action onFinish = delegate { };
+	
+	public void AddTrack(IAnimationTrack track)
+	{
+		if (duration < track.maxTime)
+			duration = track.maxTime;
+		
+		animationTracks.Add(track);
+	}
+	
+	public void RemoveTrack(IAnimationTrack track)
+	{
+		animationTracks.Remove(track);
+		
+		duration = 0;
+		foreach (var animationTrack in animationTracks)
+			if (duration < animationTrack.maxTime)
+				duration = animationTrack.maxTime;
+	}
+	
+	public void Play()
+	{
+		isRunning = true;
+	}
+	
+	public void Pause()
+	{
+		isRunning = false;
+	}
+	
+	public void Stop()
+	{
+		isRunning = false;
+		currentTime = 0;
+	}
+	
+	public void Update()
+	{
+		if (!isRunning)
+			return;
+		
+		currentTime += Time.delta;
+		
+		foreach (var animationTrack in animationTracks)
+			animationTrack.Update(currentTime);
+		
+		if (currentTime > duration)
+		{
+			if (loop)
+			{
+				currentTime = 0;
+				foreach (var animationTrack in animationTracks)
+					animationTrack.Reset();
+			}
+			else
+				isRunning = false;
+			
+			onFinish();
+		}
+	}
+}
