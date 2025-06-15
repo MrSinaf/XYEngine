a221d150cda00ce33bbcc52cf5de3ea1bbb7de3f
MrSinaf <mrsinaf@sinafproduction.xyz>
2025-06-15 23:47:06 +0200

    Refactor AnimationController structure (/ω＼)
    > Remplacement de `List<AnimationLink>` par `Dictionary<string, AnimationLink>` pour améliorer l'accès conditionnel.
    > Ajout d'une méthode `Add` dédiée aux animations nommées.
    > Mise à jour de `Set` pour accepter un `string` et simplifier l'appel aux animations par nom.
    > Révision de la déclaration `AnimationLink` en tant que classe imbriquée.
    > Modification du fichier `.csproj` pour passer la version en `preview.1`.

--- Core/Animations/AnimationController.cs
+++ Core/Animations/AnimationController.cs
@@ -2,48 +2,46 @@ namespace XYEngine.Animations;
 
 public class AnimationController
 {
-	private readonly List<AnimationLink> links = [];
+	private readonly Dictionary<string, Animation> animations = new ();
+	private readonly Dictionary<string, AnimationLink> links = new ();
 	private readonly List<AnimationLink> currents = [];
 	
 	private Animation animation;
 	
-	public void Add(AnimationLink link)
-	{
-		links.Add(link);
-	}
+	public void Add(string name, Animation animation) => animations.Add(name, animation);
+	public void Link(string begin, string end, Func<bool> condition) => links.Add(begin, new AnimationLink(animations[begin], animations[end], condition));
 	
 	public void Update()
 	{
 		animation.Update();
 		
-		for (var index = 0; index < currents.Count; index++)
-		{
-			var current = currents[index];
+		foreach (var current in currents)
 			if (current.condition())
 			{
 				Set(current.end);
 				break;
 			}
-		}
 	}
+	
+	public void Set(string animationName) => Set(animations[animationName]);
 	
 	public void Set(Animation animation)
 	{
-		animation?.Stop();
+		this.animation?.Stop();
 		this.animation = animation;
-		animation.Play();
+		this.animation.Play();
 		
 		currents.Clear();
-		foreach (var link in links)
+		foreach (var link in links.Values)
 			if (link.begin == animation)
 				currents.Add(link);
 	}
-}
-
-public class AnimationLink(Animation begin, Animation end, Func<bool> condition)
-{
-	public readonly Animation begin = begin;
-	public readonly Animation end = end;
 	
-	public readonly Func<bool> condition = condition;
+	private class AnimationLink(Animation begin, Animation end, Func<bool> condition)
+	{
+		public readonly Animation begin = begin;
+		public readonly Animation end = end;
+		
+		public readonly Func<bool> condition = condition;
+	}
 }
